7387fef39b971629abf24c7caf1e7663a16e7990
Muhammad faisal <faisal@techservicesstudio.com>
2025-12-21 16:26:03 +0500

    creating user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    fullname: { type: string, required: true },
+    email: { type: string, required: true, unique: true },
+    password: { type: string },
+    mobile: { type: string, required: true },
+    role: {
+      type: string,
+      enum: ["user", "owner", "deliveryboy"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
