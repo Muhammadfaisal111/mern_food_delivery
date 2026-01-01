4277df1782078d8db66034f73da2508c4623f627
Muhammad faisal <faisal@techservicesstudio.com>
2026-01-01 14:35:11 +0500

    sign in and sign out controllers

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { genToken } from "../utils/genToken.js";
 
-export const signup = async (req, res) => {
+export const signUp = async (req, res) => {
   try {
     const { fullName, email, password, mobile, role } = req.body;
     const user = User.findOne({ email: email });
@@ -33,6 +33,39 @@ export const signup = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ message: "User doesnot exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = await genToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+export const signOut = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({ message: "Signout successful" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
--- backend/utils/token.js
+++ backend/utils/token.js
@@ -7,8 +7,8 @@ const genToken = async (userId) => {
     });
     return token;
   } catch (error) {
-    throw new Error("Token generation failed");
     console.log(error);
+    throw new Error("Token generation failed");
   }
 };
 export default genToken;
