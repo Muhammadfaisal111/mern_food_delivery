127426ccdee64b2a1dd5aff2798a4e6ed44d9748
Muhammad faisal <faisal@techservicesstudio.com>
2026-01-02 22:00:55 +0500

    adding middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import Connectdb from "./config/db.js";
 dotenv.config();
+import Connectdb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   Connectdb();
